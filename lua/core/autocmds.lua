dd07d145c085dcb4b08ea1559baeddd616254afa
Wrik Das <wrikdas22@gmail.com>
2022-07-29 12:31:54 +0530

    organize imports on save, format on save, clean up redundant autocmds

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -5,42 +5,43 @@
 -- define autocommands with lua apis
 -- See: h:api-autocmd, h:augroup
 
-local augroup = vim.api.nvim_create_augroup   -- create/get autocommand group
-local autocmd = vim.api.nvim_create_autocmd   -- create autocommand
+local augroup = vim.api.nvim_create_augroup -- create/get autocommand group
+local autocmd = vim.api.nvim_create_autocmd -- create autocommand
 
 -- highlight on yank
 augroup('YankHighlight', { clear = true })
 autocmd('TextYankPost', {
-  group = 'YankHighlight',
-  callback = function()
-    vim.highlight.on_yank({ higroup = 'IncSearch', timeout = '200' })
-  end
+    group = 'YankHighlight',
+    callback = function()
+        vim.highlight.on_yank({ higroup = 'IncSearch', timeout = '200' })
+    end
 })
 
 -- remove whitespace on save
 autocmd('BufWritePre', {
-  pattern = '*',
-  command = ':%s/\\s\\+$//e'
+    pattern = '*',
+    command = ':%s/\\s\\+$//e'
 })
 
--- terminal settings:
--- opens terminal on the right tab
-autocmd('CmdlineEnter', {
-  command = 'command! Term :botright vsplit term://$SHELL'
+-- organize imports on save (golang)
+autocmd('BufWritePre', {
+    pattern = '*.go',
+    command = ":silent call CocAction('runCommand', 'editor.action.organizeImport')"
+})
+
+-- set sane defaults for terminal
+autocmd('TermOpen', {
+    command = 'setlocal listchars= nonumber norelativenumber nocursorline',
 })
 
 -- enter insert mode when switching to terminal
 autocmd('TermOpen', {
-  command = 'setlocal listchars= nonumber norelativenumber nocursorline',
-})
-
-autocmd('TermOpen', {
-  pattern = '*',
-  command = 'startinsert'
+    pattern = '*',
+    command = 'startinsert'
 })
 
 -- close terminal buffer on process exit
 autocmd('BufLeave', {
-  pattern = 'term://*',
-  command = 'stopinsert'
+    pattern = 'term://*',
+    command = 'stopinsert'
 })
