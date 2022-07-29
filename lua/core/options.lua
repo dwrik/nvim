606e2583d92d54a3dcd02f4907965ed54e5d03d0
Wrik Das <wrikdas22@gmail.com>
2022-07-29 12:33:30 +0530

    update tab, shift width to fix hover doc issues, disable expandtab

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -6,87 +6,85 @@
 -- see: https://neovim.io/doc/user/vim_diff.html
 -- [2] Defaults - *nvim-defaults*
 
-local g = vim.g       -- global variables
-local opt = vim.opt   -- set options (global/buffer/windows-scoped)
+local g = vim.g -- global variables
+local opt = vim.opt -- set options (global/buffer/windows-scoped)
 
 -----------------------------------------------------------
 -- general
 -----------------------------------------------------------
-opt.mouse = 'a'                                    -- enable mouse support
-opt.clipboard = opt.clipboard + { 'unnamedplus' }  -- merge system clipboard
-opt.swapfile = false                               -- don't use swapfile
-opt.completeopt = 'menuone,noinsert,noselect'      -- autocomplete options
+
+opt.mouse = 'a' -- enable mouse support
+opt.swapfile = false -- don't use swapfile
+opt.completeopt = 'menuone,noinsert,noselect' -- autocomplete options
+opt.clipboard = opt.clipboard + { 'unnamedplus' } -- merge system clipboard
 
 -----------------------------------------------------------
 -- neovim ui
 -----------------------------------------------------------
-opt.number = true           -- show line number
-opt.relativenumber = true   -- show relative line numbers
-opt.showmatch = true        -- highlight matching parenthesis
-opt.foldmethod = 'indent'   -- enable folding (default 'foldmarker')
-opt.foldlevelstart = 99     -- open all folds on start
-opt.splitright = true       -- vertical split to the right
-opt.splitbelow = true       -- horizontal split to the bottom
-opt.ignorecase = true       -- ignore case letters when search
-opt.smartcase = true        -- ignore lowercase for the whole pattern
-opt.linebreak = true        -- wrap on word boundary
-opt.termguicolors = true    -- enable 24-bit rgb colors
-opt.laststatus = 2          -- set global statusline
-opt.fillchars = 'eob: '     -- change '~' to space at end of buffer
-opt.signcolumn = 'yes'      -- always show sign column to prevent shift
 
--- opt.cmdheight = 2           -- give more space for displaying messages.
--- opt.colorcolumn = '80'      -- line lenght marker at 80 columns
-
--- change popup scrollbar thumb color (default: white)
--- vim.cmd('highlight PmenuThumb ctermbg=darkyellow guibg=darkyellow')
+opt.number = true -- line number
+opt.relativenumber = true -- relative line numbers
+opt.laststatus = 2 -- set global statusline
+opt.linebreak = true -- wrap on word boundary
+opt.termguicolors = true -- enable 24-bit rgb colors
+opt.splitright = true -- vertical split to the right
+opt.splitbelow = true -- horizontal split to the bottom
+opt.showmatch = true -- highlight matching parenthesis
+opt.ignorecase = true -- ignore case letters when search
+opt.smartcase = true -- ignore lowercase for the whole pattern
+opt.fillchars = 'eob: ' -- change '~' to space at end of buffer
+opt.signcolumn = 'yes' -- always show sign column to prevent shift
+opt.foldmethod = 'indent' -- enable folding based on indent level
+opt.foldlevelstart = 99 -- open all folds on start
 
 -----------------------------------------------------------
 -- tabs, indent
 -----------------------------------------------------------
-opt.expandtab = true        -- use spaces instead of tabs
-opt.shiftwidth = 2          -- shift 2 spaces when tab
-opt.tabstop = 2             -- 1 tab == 2 spaces
-opt.scrolloff = 2           -- offset when scroll to top/bottom
-opt.sidescrolloff = 2       -- offset when scroll to left/right
-opt.smartindent = true      -- autoindent new lines
+
+opt.tabstop = 4 -- 1 tab == 4 spaces
+opt.expandtab = true -- expand tab to spaces
+opt.scrolloff = 4 -- offset vertical scrolling
+opt.shiftwidth = 4 -- shift 4 spaces on tab
+opt.sidescrolloff = 4 -- offset horizontal scrolling
+opt.smartindent = true -- autoindent new lines
 
 -----------------------------------------------------------
 -- memory, cpu
 -----------------------------------------------------------
-opt.hidden = true           -- enable background buffers
-opt.lazyredraw = true       -- faster scrolling
-opt.updatetime = 300        -- ms to wait for trigger an event
-opt.timeoutlen = 500        -- ms to wait for keymap
+
+opt.hidden = true -- enable hidden buffers
+opt.updatetime = 300 -- event trigger timeout
+opt.timeoutlen = 500 -- keymap trigger timeout
 
 -----------------------------------------------------------
 -- startup
 -----------------------------------------------------------
+
 -- disable nvim intro
 opt.shortmess:append 'sI'
 
 -- disable builtins plugins
 local disabled_built_ins = {
-  'netrw',
-  'netrwPlugin',
-  'netrwSettings',
-  'netrwFileHandlers',
-  'gzip',
-  'zip',
-  'zipPlugin',
-  'tar',
-  'tarPlugin',
-  'getscript',
-  'getscriptPlugin',
-  'vimball',
-  'vimballPlugin',
-  '2html_plugin',
-  'logipat',
-  'rrhelper',
-  'spellfile_plugin',
-  'matchit'
+	'netrw',
+	'netrwPlugin',
+	'netrwSettings',
+	'netrwFileHandlers',
+	'gzip',
+	'zip',
+	'zipPlugin',
+	'tar',
+	'tarPlugin',
+	'getscript',
+	'getscriptPlugin',
+	'vimball',
+	'vimballPlugin',
+	'2html_plugin',
+	'logipat',
+	'rrhelper',
+	'spellfile_plugin',
+	'matchit'
 }
 
 for _, plugin in pairs(disabled_built_ins) do
-  g['loaded_' .. plugin] = 1
+	g['loaded_' .. plugin] = 1
 end
